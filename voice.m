fabf1449f0a99ef120a197c90581f692dc6cedd8
w4tashia <llyryk934@gmail.com>
2025-10-06 13:29:47 +0700

    Update voice.m

--- voice.m
+++ voice.m
@@ -4,18 +4,18 @@ clear; close all; clc;
 % Максимальная частота дискретизации в спектре моего голоса примерно 19700 Hz, следовательно требуемая для оцифровки частота дискретизации 39400 Hz
 
 %% 9) Используя библиотеки работы со звуком Matlab (или Pyton) проанализируйте имеющуюся у вас запись голоса
-[y, fs] = audioread('voice1.wav'); % Считываем файл с помошью audioread() ('voice.wav', duration - 4.7232) ('voice1.wav', duration - 9.5267)
+[y, fs] = audioread('voice.wav'); % Считываем файл с помошью audioread() ('voice.wav', duration - 4.7232) ('voice1.wav', duration - 9.5267)
 
 %% 10) Определите частоту дискретизации, которая была использована при записи голоса на цифровой носитель
 samples = length(y); % Кол-во элементов в файле
-duration = 9.5267;
+duration = 4.7232;
 fs_raschetnoe = samples / duration;
 fprintf('Count of samples: %d\n', samples);
 fprintf('Частота дискретизации посчитанная по формуле "samples / duration": %.2f Гц\n', fs_raschetnoe);
 fprintf('Настоящая частота дискретизации: %.2f Гц\n', fs);
  
 %% 11) Проредите полученный массив y с помощью функции downsample
-y1 = downsample(y, 15);          % Оставляем каждый 10-й отсчет
+y1 = downsample(y, 10);          % Оставляем каждый 10-й отсчет
 zvuk_y = audioplayer(y, fs);    % Исходный звук
 zvuk_y1 = audioplayer(y1, fs/10);% Прореженный звук
 
@@ -23,7 +23,11 @@ t = (0:length(y)-1) / fs;      % Ось времени для исходного
 t1 = (0:length(y1)-1) / (fs/10); % Ось времени для прореженного сигнала
 
 % playblocking(zvuk_y);            % Воспроизводим исходный звук и ждём окончания
-% playblocking(zvuk_y1);           % Потом прореженный звук
+% playblocking(zvuk_y1);          % Потом прореженный звук
+
+filename = 'my_audio1.wav';
+Fs = 44100/10;
+audiowrite(filename, y1, Fs);
 
 figure
 subplot(2,1,1)
@@ -45,17 +49,17 @@ y_spectrum_down = fft(y1); % Спектр сигнала в комплексно
 N_orig = length(y); % Число samples оригинальной версии
 N_down = length(y1); % Число samples downsample версии
 
-f_axis_orig = (0:N_orig-1)*(fs/N_orig); % Шкала частот
-f_axis_down = (0:N_down-1)*(fs/N_down); % Шкала частот
+f_axis_orig = (0:N_orig-1)*(fs/N_orig); % Шкала частот оригинальной версии
+f_axis_down = (0:N_down-1)*(fs/N_down); % Шкала частот 
 
-figure % Открываем новое окно для графика
+figure
 plot(f_axis_orig, abs(y_spectrum_orig)/N_orig); % Визуализируем амплитудный спект оригинального сигнала
 xlabel('Частота')
 ylabel('Амплитуда')
 title('Амплитудный спектр оригинального сигнала, fs = 44100 Hz')
 grid on;
 
-figure % Открываем новое окно для графика
+figure
 plot(f_axis_down, abs(y_spectrum_down)/N_down); % Визуализируем амплитудный спект downsample сигнала
 xlabel('Частота')
 ylabel('Амплитуда')
@@ -79,14 +83,12 @@ y_normis = y / max(y);
 figure;
 for i = 1:length(bits_list)
     bits = bits_list(i);
-    yq = quantize_signal(y_normis, bits);
+    [yq_r, yq_n] = quantize_signal(y_normis, bits);
 
-    % Ошибка квантования
-    errors(i) = mean(abs(y_normis - yq));
+    errors(i) = mean(abs(y_normis - yq_n));
 
-    % Визуализация
     subplot(length(bits_list),1,i);
-    plot(t, yq);
+    plot(t, yq_r);
     title(['Сигнал после квантования, ', num2str(bits), ' бит']);
     xlabel('Время, с');
     ylabel('Амплитуда');
@@ -95,9 +97,10 @@ end
 disp('Средние ошибки квантования:')
 disp(table(bits_list', errors', 'VariableNames', {'Разрядность', 'Средняя_ошибка'}))
 
-function yq = quantize_signal(y, bits)
+function [yq_r, yq_n] = quantize_signal(y, bits)
     levels = 2^bits; % количество уровней
     y_norm = (y + 1) / 2; % перевод диапазона [-1,1] -> [0,1]
     y_scaled = y_norm * (levels - 1); % масштабирование под уровни
-    yq = round(y_scaled)/max(y_scaled) * 2 - 1; % округление до ближайшего уровня
+    yq_r = round(y_scaled); % округление до ближайшего уровня
+    yq_n = round(y_scaled)/max(y_scaled) * 2 - 1;
 end
