43d7a0e510cb3072305e0bb22a2975e2d07becc2
someotb <llyryk934@gmail.com>
2025-12-15 20:51:05 +0700

    fix: distant condition

--- lab2.m
+++ lab2.m
@@ -26,10 +26,10 @@ Lclutter = -(4.78 * (log10(frequency))^2 - 18.33 * log10(frequency) + 40.94);
 
 s = zeros(size(distance));
 
-idx_ge_1 = distance >= 1000;
+idx_ge_1 = distance >= 1;
 s(idx_ge_1) = 44.9 - 6.55 * log10(frequency);
 
-idx_lt_1 = distance < 1000;
+idx_lt_1 = distance < 1;
 s(idx_lt_1) = (47.88 + 13.9 * log10(frequency) - 13.9 * log10(hBS)) * (1 / log10(50));
 
 PL = A + B * log10(frequency) - 13.82 * log10(hBS) - a + s .* log10(distance) + Lclutter;
@@ -39,12 +39,12 @@ max_dist = zeros(size(MAPL_DL));
 for i = 1 : length(MAPL_DL)
     diff = abs(MAPL_DL(i) - PL);
     [~, idx] = min(diff);
-    max_dist(i) = distance(idx)^2 * 1.95;
+    max_dist(i) = distance(idx);
 end
 
 figure;
 plot(temrature, max_dist);
 title("Зависимости радиуса соты от температуры")
 xlabel("Temprature (K)")
-ylabel("Squre (km^2)");
+ylabel("Radius (m)");
 grid("on");
